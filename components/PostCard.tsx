d6c21e36dd9fa42ca951cc21f1e2b40143327868
Mohammed Tanvir <tanvir.eatl@gmail.com>
2026-03-20 22:49:20 +0600

    Fix: renderHTML component wrapped into new reusable component

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -7,7 +7,6 @@ import { Router } from 'expo-router'
 import React from 'react'
 import { Pressable, StyleSheet, Text, View } from 'react-native'
 import { useWindowDimensions } from 'react-native'
-import RenderHtml from 'react-native-render-html'
 
 import { theme } from '@/constants/theme'
 import { heightPercentage } from '@/helpers/common'
@@ -16,13 +15,7 @@ import { PostRow } from '@/utils/databases/post'
 import { UserProfileRow } from '@/utils/databases/types/userProfile.types'
 
 import Avatar from './Avatar'
-
-const baseStyle = {
-  color: theme.colors.primaryDark,
-  fontSize: heightPercentage(1.75),
-  fontFamily: 'System',
-  lineHeight: 22,
-}
+import PostDetailsViewer from './PostDetailsViewer'
 
 const PostCard = ({
   post,
@@ -45,74 +38,7 @@ const PostCard = ({
         </View>
         <Entypo name="dots-three-horizontal" size={16} color={theme.colors.textLight} />
       </View>
-      {post.body && (
-        <View>
-          <RenderHtml
-            contentWidth={width}
-            source={{ html: post.body }}
-            baseStyle={baseStyle}
-            tagsStyles={{
-              p: {
-                marginVertical: 6,
-                color: theme.colors.primaryDark,
-                fontFamily: 'System',
-                fontSize: heightPercentage(1.75),
-              },
-              h1: {
-                fontSize: heightPercentage(2.5),
-                fontWeight: 700,
-                color: theme.colors.primaryDark,
-                marginVertical: 10,
-              },
-              h4: {
-                fontSize: heightPercentage(2),
-                fontWeight: 600,
-                color: theme.colors.primaryDark,
-                marginVertical: 8,
-              },
-              b: { fontWeight: 700 },
-              strong: { fontWeight: 700 },
-              i: { fontStyle: 'italic' },
-              em: { fontStyle: 'italic' },
-              u: { textDecorationLine: 'underline' },
-              s: { textDecorationLine: 'line-through' },
-              strike: { textDecorationLine: 'line-through' },
-              blockquote: {
-                borderLeftWidth: 4,
-                borderLeftColor: theme.colors.primary,
-                paddingLeft: 8,
-                marginVertical: 8,
-                fontStyle: 'italic',
-                color: theme.colors.text,
-              },
-              pre: {
-                backgroundColor: '#f5f5f5',
-                padding: 6,
-                borderRadius: 4,
-                fontFamily: 'Courier',
-              },
-              code: {
-                backgroundColor: '#f5f5f5',
-                paddingHorizontal: 4,
-                borderRadius: 4,
-                fontFamily: 'Courier',
-              },
-              ul: { paddingLeft: 20, marginVertical: 4 },
-              ol: { paddingLeft: 20, marginVertical: 4 },
-              li: { marginVertical: 2 },
-              '.ql-align-left': { textAlign: 'left' },
-              '.ql-align-center': { textAlign: 'center' },
-              '.ql-align-right': { textAlign: 'right' },
-              hr: {
-                borderBottomWidth: 1,
-                borderBottomColor: theme.colors.textLight,
-                marginVertical: 8,
-              },
-            }}
-            ignoredStyles={['fontFamily', 'fontSize', 'lineHeight']}
-          />
-        </View>
-      )}
+      {post.body && <PostDetailsViewer containerWidth={width} html={post.body} />}
       {post.file?.includes('image') && (
         <Image source={post.file} contentFit="cover" style={styles.filePreview} />
       )}
---
+++ components/PostDetailsViewer.tsx
@@ -0,0 +1,92 @@
+import React from 'react'
+import { View } from 'react-native'
+import RenderHtml, { HTMLSourceInline } from 'react-native-render-html'
+
+import { theme } from '@/constants/theme'
+import { heightPercentage } from '@/helpers/common'
+
+const baseStyle = {
+  color: theme.colors.primaryDark,
+  fontSize: heightPercentage(1.75),
+  fontFamily: 'System',
+  lineHeight: 22,
+}
+
+const PostDetailsViewer = ({
+  html,
+  containerWidth,
+}: {
+  containerWidth?: number
+  html: HTMLSourceInline['html']
+}) => {
+  return (
+    <View>
+      <RenderHtml
+        contentWidth={containerWidth}
+        source={{ html }}
+        baseStyle={baseStyle}
+        tagsStyles={{
+          p: {
+            marginVertical: 6,
+            color: theme.colors.primaryDark,
+            fontFamily: 'System',
+            fontSize: heightPercentage(1.75),
+          },
+          h1: {
+            fontSize: heightPercentage(2.5),
+            fontWeight: 700,
+            color: theme.colors.primaryDark,
+            marginVertical: 10,
+          },
+          h4: {
+            fontSize: heightPercentage(2),
+            fontWeight: 600,
+            color: theme.colors.primaryDark,
+            marginVertical: 8,
+          },
+          b: { fontWeight: 700 },
+          strong: { fontWeight: 700 },
+          i: { fontStyle: 'italic' },
+          em: { fontStyle: 'italic' },
+          u: { textDecorationLine: 'underline' },
+          s: { textDecorationLine: 'line-through' },
+          strike: { textDecorationLine: 'line-through' },
+          blockquote: {
+            borderLeftWidth: 4,
+            borderLeftColor: theme.colors.primary,
+            paddingLeft: 8,
+            marginVertical: 8,
+            fontStyle: 'italic',
+            color: theme.colors.text,
+          },
+          pre: {
+            backgroundColor: '#f5f5f5',
+            padding: 6,
+            borderRadius: 4,
+            fontFamily: 'Courier',
+          },
+          code: {
+            backgroundColor: '#f5f5f5',
+            paddingHorizontal: 4,
+            borderRadius: 4,
+            fontFamily: 'Courier',
+          },
+          ul: { paddingLeft: 20, marginVertical: 4 },
+          ol: { paddingLeft: 20, marginVertical: 4 },
+          li: { marginVertical: 2 },
+          '.ql-align-left': { textAlign: 'left' },
+          '.ql-align-center': { textAlign: 'center' },
+          '.ql-align-right': { textAlign: 'right' },
+          hr: {
+            borderBottomWidth: 1,
+            borderBottomColor: theme.colors.textLight,
+            marginVertical: 8,
+          },
+        }}
+        ignoredStyles={['fontFamily', 'fontSize', 'lineHeight']}
+      />
+    </View>
+  )
+}
+
+export default PostDetailsViewer
