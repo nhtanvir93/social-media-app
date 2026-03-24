5ade6e294b21f4bfd09797bdb7416749f1de7af9
Mohammed Tanvir <tanvir.eatl@gmail.com>
2026-03-24 10:15:47 +0600

    Fix: some unused files removed for database types and old database type modified

--- utils/databases/types/database.types.ts
+++ utils/databases/types/database.types.ts
@@ -1,36 +1,336 @@
-import { PostPayload, PostRow } from './post.types'
-import { UserProfileInsert, UserProfileRow, UserProfileUpdate } from './userProfile.types'
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
 
 export type Database = {
+  // Allows to automatically instantiate createClient with right options
+  // instead of createClient<Database, { PostgrestVersion: 'XX' }>(URL, KEY)
+  __InternalSupabase: {
+    PostgrestVersion: '14.4'
+  }
   public: {
     Tables: {
-      users: {
-        Row: UserProfileRow
-        Insert: UserProfileInsert
-        Update: UserProfileUpdate
-        Relationships: []
-      }
-      posts: {
-        Row: PostRow
-        Insert: PostPayload
-        Update: PostPayload
+      comments: {
+        Row: {
+          created_at: string | null
+          id: string
+          postId: string
+          text: string
+          userId: string
+        }
+        Insert: {
+          created_at?: string | null
+          id?: string
+          postId: string
+          text: string
+          userId: string
+        }
+        Update: {
+          created_at?: string | null
+          id?: string
+          postId?: string
+          text?: string
+          userId?: string
+        }
         Relationships: [
           {
-            foreignKeyName: 'posts_userId_fkey'
+            foreignKeyName: 'comments_postId_fkey'
+            columns: ['postId']
+            isOneToOne: false
+            referencedRelation: 'posts'
+            referencedColumns: ['id']
+          },
+          {
+            foreignKeyName: 'comments_userId_fkey'
             columns: ['userId']
+            isOneToOne: false
             referencedRelation: 'users'
             referencedColumns: ['id']
           },
         ]
       }
+      notifications: {
+        Row: {
+          created_at: string | null
+          data: Json | null
+          id: string
+          receiverId: string
+          senderId: string
+          title: string
+        }
+        Insert: {
+          created_at?: string | null
+          data?: Json | null
+          id?: string
+          receiverId: string
+          senderId: string
+          title: string
+        }
+        Update: {
+          created_at?: string | null
+          data?: Json | null
+          id?: string
+          receiverId?: string
+          senderId?: string
+          title?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: 'notifications_receiverId_fkey'
+            columns: ['receiverId']
+            isOneToOne: false
+            referencedRelation: 'users'
+            referencedColumns: ['id']
+          },
+          {
+            foreignKeyName: 'notifications_senderId_fkey'
+            columns: ['senderId']
+            isOneToOne: false
+            referencedRelation: 'users'
+            referencedColumns: ['id']
+          },
+        ]
+      }
+      postLikes: {
+        Row: {
+          createdAt: string | null
+          id: string
+          postId: string
+          userId: string
+        }
+        Insert: {
+          createdAt?: string | null
+          id?: string
+          postId: string
+          userId: string
+        }
+        Update: {
+          createdAt?: string | null
+          id?: string
+          postId?: string
+          userId?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: 'post_likes_postId_fkey'
+            columns: ['postId']
+            isOneToOne: false
+            referencedRelation: 'posts'
+            referencedColumns: ['id']
+          },
+          {
+            foreignKeyName: 'post_likes_userId_fkey'
+            columns: ['userId']
+            isOneToOne: false
+            referencedRelation: 'users'
+            referencedColumns: ['id']
+          },
+        ]
+      }
+      posts: {
+        Row: {
+          body: string | null
+          createdAt: string | null
+          file: string | null
+          id: string
+          userId: string
+        }
+        Insert: {
+          body?: string | null
+          createdAt?: string | null
+          file?: string | null
+          id?: string
+          userId: string
+        }
+        Update: {
+          body?: string | null
+          createdAt?: string | null
+          file?: string | null
+          id?: string
+          userId?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: 'posts_userId_fkey'
+            columns: ['userId']
+            isOneToOne: false
+            referencedRelation: 'users'
+            referencedColumns: ['id']
+          },
+        ]
+      }
+      users: {
+        Row: {
+          address: string | null
+          bio: string | null
+          created_at: string | null
+          email: string | null
+          id: string
+          image: string | null
+          name: string | null
+          phoneNumber: string | null
+        }
+        Insert: {
+          address?: string | null
+          bio?: string | null
+          created_at?: string | null
+          email?: string | null
+          id: string
+          image?: string | null
+          name?: string | null
+          phoneNumber?: string | null
+        }
+        Update: {
+          address?: string | null
+          bio?: string | null
+          created_at?: string | null
+          email?: string | null
+          id?: string
+          image?: string | null
+          name?: string | null
+          phoneNumber?: string | null
+        }
+        Relationships: []
+      }
     }
-    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-    Views: {}
-    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-    Functions: {}
-    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-    Enums: {}
-    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-    CompositeTypes: {}
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
   }
 }
+
+type DatabaseWithoutInternals = Omit<Database, '__InternalSupabase'>
+
+type DefaultSchema = DatabaseWithoutInternals[Extract<keyof Database, 'public'>]
+
+export type Tables<
+  DefaultSchemaTableNameOrOptions extends
+    | keyof (DefaultSchema['Tables'] & DefaultSchema['Views'])
+    | { schema: keyof DatabaseWithoutInternals },
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals
+  }
+    ? keyof (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions['schema']]['Tables'] &
+        DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions['schema']]['Views'])
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions['schema']]['Tables'] &
+      DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions['schema']]['Views'])[TableName] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : DefaultSchemaTableNameOrOptions extends keyof (DefaultSchema['Tables'] &
+        DefaultSchema['Views'])
+    ? (DefaultSchema['Tables'] &
+        DefaultSchema['Views'])[DefaultSchemaTableNameOrOptions] extends {
+        Row: infer R
+      }
+      ? R
+      : never
+    : never
+
+export type TablesInsert<
+  DefaultSchemaTableNameOrOptions extends
+    | keyof DefaultSchema['Tables']
+    | { schema: keyof DatabaseWithoutInternals },
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals
+  }
+    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions['schema']]['Tables']
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions['schema']]['Tables'][TableName] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema['Tables']
+    ? DefaultSchema['Tables'][DefaultSchemaTableNameOrOptions] extends {
+        Insert: infer I
+      }
+      ? I
+      : never
+    : never
+
+export type TablesUpdate<
+  DefaultSchemaTableNameOrOptions extends
+    | keyof DefaultSchema['Tables']
+    | { schema: keyof DatabaseWithoutInternals },
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals
+  }
+    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions['schema']]['Tables']
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions['schema']]['Tables'][TableName] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema['Tables']
+    ? DefaultSchema['Tables'][DefaultSchemaTableNameOrOptions] extends {
+        Update: infer U
+      }
+      ? U
+      : never
+    : never
+
+export type Enums<
+  DefaultSchemaEnumNameOrOptions extends
+    | keyof DefaultSchema['Enums']
+    | { schema: keyof DatabaseWithoutInternals },
+  EnumName extends DefaultSchemaEnumNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals
+  }
+    ? keyof DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions['schema']]['Enums']
+    : never = never,
+> = DefaultSchemaEnumNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions['schema']]['Enums'][EnumName]
+  : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema['Enums']
+    ? DefaultSchema['Enums'][DefaultSchemaEnumNameOrOptions]
+    : never
+
+export type CompositeTypes<
+  PublicCompositeTypeNameOrOptions extends
+    | keyof DefaultSchema['CompositeTypes']
+    | { schema: keyof DatabaseWithoutInternals },
+  CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals
+  }
+    ? keyof DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions['schema']]['CompositeTypes']
+    : never = never,
+> = PublicCompositeTypeNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions['schema']]['CompositeTypes'][CompositeTypeName]
+  : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema['CompositeTypes']
+    ? DefaultSchema['CompositeTypes'][PublicCompositeTypeNameOrOptions]
+    : never
+
+export const Constants = {
+  public: {
+    Enums: {},
+  },
+} as const
